9d4b9024cf2a9f093707e6e479f1f893e8be8101
Jae <gjwodud119@gmail.com>
2022-04-16 23:26:41 +0900

    Fix: 구조 개선, 변수명 가독성 고려 수정

--- routes/game.js
+++ routes/game.js
@@ -8,48 +8,57 @@ const message = {
     에러 : "이상한거 내지 마세요 ㅡㅡ"
 };
 
-const hands = {
-    묵: 0,
-    찌: 1,
-    빠: 2
-};
-
-router.get("/:yours", (req, res) => {
-    const getRandom = () => Math.floor(Math.random() * 3)
-
-    const yours = req.params['yours'];
-    const ours = Object.keys(hands)[getRandom()];
-
-    try{
-        const result = getGameResult(hands[yours], hands[ours]);
-
-        res.status(200).json({
-                status: result,
-                yours,
-                ours,
-                message: message[result]
-        });
-    } catch (e) {
-        res.status(400).json({
-            status: "오류",
-            message: message.에러
-        });
+router.get("/:playerHand", (req, res) => {
+    const playerHand = req.params['playerHand'];
+    if(!validateHand(playerHand)) {
+        return sendError(res, message.에러);
     }
+
+    const serverHand = getRandomHand();
+    const result = getGameResult(playerHand, serverHand);
+
+    res.status(200).json({
+        result,
+        playerHand,
+        serverHand,
+        message: message[result]
+    });
 });
 
-function getGameResult(users, servers) {
-    switch (users - servers) {
-        case 2:
-        case -1:
-            return "승리"
-        case 0:
-            return "무승부"
-        case -2:
-        case 1 :
-            return "패배"
-        default :
-            throw Error(message.에러)
+function getGameResult(playerHand, serverHand) {
+    if(playerHand === serverHand) return "무승부"
+
+    if(playerHand === "묵" && serverHand === "찌"
+        || playerHand === "찌" && serverHand === "빠"
+        || playerHand === "빠" && serverHand === "묵") return "승리"
+
+    return "패배"
+}
+
+function getRandomHand() {
+    switch (getRandom(3)) {
+        case 0: return "묵"
+        case 1: return "찌"
+        case 2: return "빠"
     }
+}
+
+function getRandom(maxNum) {
+    return Math.floor(Math.random() * maxNum)
+}
+
+function validateHand(playerHand) {
+    if(playerHand !== undefined && (playerHand === "묵"
+        || playerHand === "찌" || playerHand === "빠")) return true;
+
+    return false;
+}
+
+function sendError(res, errorMessage) {
+    return res.status(400).json({
+        status: "에러",
+        errorMessage
+    });
 }
 
 module.exports = router;
